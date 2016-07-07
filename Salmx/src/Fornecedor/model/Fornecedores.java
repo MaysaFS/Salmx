d83192841525d1372268cbe03e8e92f1a38476ca
Thau26 <thauanyaraujoalves@hotmail.com>
2016-07-07 16:46:20 -0300

    modificações de Thauany

---
+++ Salmx/src/Fornecedor/model/Fornecedores.java
@@ -0,0 +1,112 @@
+
+package Fornecedor.model;
+
+
+public class Fornecedores {
+    
+    String razaosocial;
+    String inscricaoestadual;
+    String cnpj;
+    String telefone;
+    String rua;
+    String numero;
+    String bairro;
+    String cidade;
+    String uf;
+    String categoria;
+    int codigo;
+
+    public String getRazaosocial() {
+        return razaosocial;
+    }
+
+    public void setRazaosocial(String razaosocial) {
+        this.razaosocial = razaosocial;
+    }
+
+    public String getInscricaoestadual() {
+        return inscricaoestadual;
+    }
+
+    public void setInscricaoestadual(String inscricaoestadual) {
+        this.inscricaoestadual = inscricaoestadual;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Fornecedores() {
+    }
+    
+    
+    
+}
